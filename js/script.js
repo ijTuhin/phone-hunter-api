30637c086d0e7a948743b530441aa6eef89c11f3
ijTuhin <isratishu0209@gmail.com>
2022-03-01 03:38:21 +0600

    display error message for wrong data search

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ document.getElementById('error-message1').style.display = 'none';
 document.getElementById('error-message2').style.display = 'none';
 
 // getting results from search box
-const getResults = phone => {
+const getResults = () => {
     const searchBox = document.getElementById('search-box');
     const searchBoxValue = searchBox.value;
     // console.log(searchBox.value, 'search box checked');
+
     //showing error message for empty search attempt
     if (searchBoxValue == '') {
         document.getElementById('error-message1').style.display = 'block';
@@ -17,5 +18,13 @@ const getResults = phone => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchBoxValue}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayResults(data));
+}
+// displaying searched result
+const displayResults = phone => {
+    // console.log(phone.status);
+    // showing error message for wrong search
+    if (phone.status == false) {
+        document.getElementById('error-message2').style.display = 'block';
+    }
 }
