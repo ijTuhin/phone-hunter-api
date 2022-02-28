430f276e96cf647b5124edd40214a96e58c6e92b
ijTuhin <isratishu0209@gmail.com>
2022-03-01 03:23:48 +0600

    showing error message for empty search attempts

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-// hiding error message
-document.getElementById('error-message').style.display = 'none';
+// hiding error messages
+document.getElementById('error-message1').style.display = 'none';
+document.getElementById('error-message2').style.display = 'none';
+
 // getting results from search box
 const getResults = phone => {
     const searchBox = document.getElementById('search-box');
-    console.log(searchBox.value, 'search box checked');
+    const searchBoxValue = searchBox.value;
+    // console.log(searchBox.value, 'search box checked');
+    //showing error message for empty search attempt
+    if (searchBoxValue == '') {
+        document.getElementById('error-message1').style.display = 'block';
+    }
+    // clear the input field
     searchBox.value = '';
+    // fetching data from api
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchBoxValue}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
 }
