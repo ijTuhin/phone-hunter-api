9abd415ccc3c16995634d4747ae784742dcd13bc
ijTuhin <isratishu0209@gmail.com>
2022-03-01 03:10:28 +0600

    event handle on search-box button & get input-value from search-box

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+// hiding error message
 document.getElementById('error-message').style.display = 'none';
+// getting results from search box
+const getResults = phone => {
+    const searchBox = document.getElementById('search-box');
+    console.log(searchBox.value, 'search box checked');
+    searchBox.value = '';
+}
