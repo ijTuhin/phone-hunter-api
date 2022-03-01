19a3b36d636075d345c41b2846af7177b51423a5
ijTuhin <isratishu0209@gmail.com>
2022-03-01 14:13:50 +0600

    displayed sensor & others

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ document.getElementById('error-message2').style.display = 'none';
 const getResults = () => {
     const searchBox = document.getElementById('search-box');
     const searchBoxValue = searchBox.value;
-    // console.log(searchBox.value, 'search box checked');
 
     if (searchBoxValue == '') {
         //showing error message for empty search attempt
@@ -28,7 +27,7 @@ const getResults = () => {
 }
 // -------------------*********** displaying searched result
 const displayResults = phones => {
-    // showing error message for wrong search
+    // showing error message for wrong data search
     if (phones.status == false) {
         document.getElementById('error-message2').style.display = 'block';
     }
@@ -36,13 +35,13 @@ const displayResults = phones => {
     const resultArea = document.getElementById('result-area');
     resultArea.textContent = ''; // to remove previous search result
     const phoneData = phones.data;
-    phoneData.forEach(phone => {
+    const phoneDataFirst20 = phoneData.slice(0, 20);
+    phoneDataFirst20.forEach(phone => {
         document.getElementById('error-message2').style.display = 'none';
-        // console.log(phone);
         const eachResultDiv = document.createElement('div');
         eachResultDiv.classList.add('each-result');
         eachResultDiv.innerHTML = `
-        <div class="border border-none rounded-lg flex flex-col justify-center bg-slate-50">
+            <div class="border border-none rounded-lg flex flex-col justify-center bg-slate-50">
                     <!-- Phone image -->
                     <div class="px-3 py-12 flex justify-center">
                         <img id="result-image" class="md:w-auto md:h-auto"
@@ -61,38 +60,34 @@ const displayResults = phones => {
                             <i class="material-icons text-cyan-600 hover:text-white">arrow_forward</i></button>
                         </a>
                     </div>
-        </div>
-                `;
+            </div>
+        `;
         resultArea.appendChild(eachResultDiv);
-        // console.log(phone.slug);
     })
 }
 
 
 // -------------------*********** getting details from details button
-
 const getDetails = phoneId => {
-    // console.log(phoneId);
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayDetails(data));
 }
 const displayDetails = details => {
-    console.log(details.data.others);
     const detailArea = document.getElementById('display-detail');
     const eachDetails = document.createElement('div');
     eachDetails.classList.add('eachDetail');
     detailArea.appendChild(eachDetails);
-    if (details.data.releaseDate == '') {
-        // details.data.releaseDate = '';
-        console.log('Israt', details.data.releaseDate, 'Tuhin');
+
+    // for no releaseDate & no other info
+    if (details.data.releaseDate == '' && details.data.others == undefined) {
         eachDetails.innerHTML = `
-        <div class="grid md:grid-cols-3 gap-0 my-5">
+                <div class="grid md:grid-cols-3 gap-0 my-5">
                     <div class="border md:rounded-l-lg md:rounded-tr-none rounded-t-lg border-slate-300 bg-slate-50 md:p-2 p-3 flex md:justify-center justify-start content-center">
                         <img id="result-image" class="w-auto h-auto" src="${details.data.image}" alt="">
                     </div>
-                    <div class="border border-slate-300 md:p-2 p-2">
+                    <div class="border border-slate-300 md:col-span-2 md:p-2 p-2">
                             <p id="phone-name" class="text-2xl font-normal md:px-3 p-2">${details.data.name}</p>
                             <p id="release-date" class="text-lg font-normal md:px-3 p-2">No release date found</p>
                             <hr>
@@ -102,10 +97,34 @@ const displayDetails = details => {
                                 <p>Memory: <span class="text-sm">${details.data.mainFeatures.memory}</span></p>
                                 <p>Display-size: <span class="text-sm">${details.data.mainFeatures.displaySize}</span></p>
                                 <p>Chip-set: <span class="text-sm">${details.data.mainFeatures.chipSet}</span></p>
-                                <p>Sensors:</p>
+                                <p>Sensors: <span class="text-sm"> ${details.data.mainFeatures.sensors}</span></p>
                             </div>
                     </div>
-                    <div class="border border-slate-300 md:p-2 p-2">
+                </div>
+            `;
+    }
+
+    // for no releaseDate info
+    else if (details.data.releaseDate == '') {
+        console.log('Israt', details.data.releaseDate, 'Tuhin');
+        eachDetails.innerHTML = `
+                <div class="grid md:grid-cols-3 gap-0 my-5">
+                    <div class="border md:rounded-l-lg md:rounded-tr-none rounded-t-lg border-slate-300 bg-slate-50 md:p-2 p-3 flex md:justify-center justify-start content-center">
+                        <img id="result-image" class="w-auto h-auto" src="${details.data.image}" alt="">
+                    </div>
+                    <div class="border border-slate-300 md:col-span-2 md:p-2 p-2">
+                            <p id="phone-name" class="text-2xl font-normal md:px-3 p-2">${details.data.name}</p>
+                            <p id="release-date" class="text-lg font-normal md:px-3 p-2">No release date found</p>
+                            <hr>
+                            <div class="text-lg font-base md:px-3 p-2">
+                            <p id="main-feature" class="text-xl font-normal">Main features:</p>
+                                <p>Storage: <span class="text-sm">${details.data.mainFeatures.storage}</span></p>
+                                <p>Memory: <span class="text-sm">${details.data.mainFeatures.memory}</span></p>
+                                <p>Display-size: <span class="text-sm">${details.data.mainFeatures.displaySize}</span></p>
+                                <p>Chip-set: <span class="text-sm">${details.data.mainFeatures.chipSet}</span></p>
+                                <p>Sensors: <span class="text-sm"> ${details.data.mainFeatures.sensors}</span></p>
+                            </div>
+                            <hr>
                             <div class="text-lg font-base md:px-3 px-2 pt-0">
                             <p id="others" class="text-xl font-normal">Others:</p>
                                 <p>Bluetooth: <span class="text-sm">${details.data.others.Bluetooth}</span></p>
@@ -116,17 +135,19 @@ const displayDetails = details => {
                                 <p>WLAN: <span class="text-sm">${details.data.others.WLAN}</span></p>
                             </div>
                     </div>
-        </div>
-        `;
+                </div>
+            `;
     }
+
+    // for no others info
     else if (details.data.others == undefined) {
         console.log('Others item not found');
         eachDetails.innerHTML = `
-        <div class="grid md:grid-cols-3 gap-0 my-5">
+                <div class="grid md:grid-cols-3 gap-0 my-5">
                     <div class="border md:rounded-l-lg md:rounded-tr-none rounded-t-lg border-slate-300 bg-slate-50 md:p-2 p-3 flex md:justify-center justify-start content-center">
                         <img id="result-image" class="w-auto h-auto" src="${details.data.image}" alt="">
                     </div>
-                    <div class="border border-slate-300 md:p-2 p-2">
+                    <div class="border border-slate-300 md:col-span-2 md:p-2 p-2">
                             <p id="phone-name" class="text-2xl font-normal md:px-3 p-2">${details.data.name}</p>
                             <p id="release-date" class="text-lg font-normal md:px-3 p-2">Release date: ${details.data.releaseDate}</p>
                             <hr>
@@ -136,20 +157,20 @@ const displayDetails = details => {
                                 <p>Memory: <span class="text-sm">${details.data.mainFeatures.memory}</span></p>
                                 <p>Display-size: <span class="text-sm">${details.data.mainFeatures.displaySize}</span></p>
                                 <p>Chip-set: <span class="text-sm">${details.data.mainFeatures.chipSet}</span></p>
-                                <p>Sensors:</p>
+                                <p>Sensors: <span class="text-sm"> ${details.data.mainFeatures.sensors}</span></p>
                             </div>
                     </div>
-        </div>
-        `;
+                </div>
+            `;
     }
+    // for all info
     else {
-        // console.log(details.data.releaseDate);
         eachDetails.innerHTML = `
-        <div class="grid md:grid-cols-3 gap-0 my-5">
+                <div class="grid md:grid-cols-3 gap-0 my-5">
                     <div class="border md:rounded-l-lg md:rounded-tr-none rounded-t-lg border-slate-300 bg-slate-50 md:p-2 p-3 flex md:justify-center justify-start content-center">
                         <img id="result-image" class="w-auto h-auto" src="${details.data.image}" alt="">
                     </div>
-                    <div class="border border-slate-300 md:p-2 p-2">
+                    <div class="border border-slate-300 md:col-span-2 md:p-2 p-2">
                             <p id="phone-name" class="text-2xl font-normal md:px-3 p-2">${details.data.name}</p>
                             <p id="release-date" class="text-lg font-normal md:px-3 p-2">Release date: ${details.data.releaseDate}</p>
                             <hr>
@@ -159,10 +180,9 @@ const displayDetails = details => {
                                 <p>Memory: <span class="text-sm">${details.data.mainFeatures.memory}</span></p>
                                 <p>Display-size: <span class="text-sm">${details.data.mainFeatures.displaySize}</span></p>
                                 <p>Chip-set: <span class="text-sm">${details.data.mainFeatures.chipSet}</span></p>
-                                <p>Sensors:</p>
+                                <p>Sensors: <span class="text-sm">${details.data.mainFeatures.sensors}</span></p>
                             </div>
-                    </div>
-                    <div class="border border-slate-300 md:p-2 p-2">
+                            <hr>
                             <div class="text-lg font-base md:px-3 px-2 pt-0">
                             <p id="others" class="text-xl font-normal">Others:</p>
                                 <p>Bluetooth: <span class="text-sm">${details.data.others.Bluetooth}</span></p>
@@ -170,10 +190,10 @@ const displayDetails = details => {
                                 <p>NFC: <span class="text-sm">${details.data.others.NFC}</span></p>
                                 <p>Radio: <span class="text-sm">${details.data.others.Radio}</span></p>
                                 <p>USB: <span class="text-sm">${details.data.others.USB}</span></p>
-                                <p>WLAN: <span class="text-sm">${details.data.others.WLAN}</span></p>
+                                <p>WLAN: <span class="text-sm"> ${details.data.others.WLAN}</span></p>
                             </div>
                     </div>
-        </div>
-        `;
+                </div>
+            `;
     }
 }
