b440e6ad9859d8539876df36189a33669af2741d
ijTuhin <isratishu0209@gmail.com>
2022-03-01 12:39:13 +0600

    some changes in margin & padding

--- js/script.js
+++ js/script.js
@@ -44,20 +44,22 @@ const displayResults = phones => {
         eachResultDiv.innerHTML = `
         <div class="border border-none rounded-lg flex flex-col justify-center bg-slate-50">
                     <!-- Phone image -->
-                    <div class="p-3 flex justify-center">
+                    <div class="px-3 py-12 flex justify-center">
                         <img id="result-image" class="md:w-auto md:h-auto"
                             src="${phone.image}" alt="">
                     </div>
                     <!-- phone name & brand name -->
-                    <div>
-                        <p id="phone-name" class="text-xl font-normal px-3">${phone.phone_name}</p>
-                        <p id="brand-name" class="text-lg font-normal px-3">${phone.brand}</p>
+                    <div class="pt-4">
+                        <p id="phone-name" class="text-xl font-normal px-3 pl-3">${phone.phone_name}</p>
+                        <p id="brand-name" class="text-lg font-normal px-3 pl-3">${phone.brand}</p>
                     </div>
                     <!-- details button -->
-                    <div class="p-3 flex justify-start">
-                        <button onclick="getDetails('${phone.slug}')" id="detail-button"
+                    <div class="p-3 pl-3 flex justify-start">
+                        <a href="#top-section" class="no-underline">
+                            <button onclick="getDetails('${phone.slug}')" id="detail-button"
                             class="border border-none rounded w-28 p-2 shadow-sm focus:outline-none focus:ring-1 flex justify-between bg-cyan-500/[0.4] hover:bg-cyan-600 hover:text-white">Details
                             <i class="material-icons text-cyan-600 hover:text-white">arrow_forward</i></button>
+                        </a>
                     </div>
         </div>
                 `;
@@ -77,10 +79,13 @@ const getDetails = phoneId => {
         .then(data => displayDetails(data));
 }
 const displayDetails = details => {
-    console.log(details.data);
+    console.log(details.data.others);
     if (details.data.releaseDate == '') {
         // details.data.releaseDate = '';
         console.log('Israt', details.data.releaseDate, 'Tuhin');
+    }
+    else if (details.data.others == undefined) {
+        console.log('Others item not found')
     }
     else {
         // console.log(details.data.releaseDate);
@@ -88,37 +93,33 @@ const displayDetails = details => {
         const eachDetails = document.createElement('div');
         eachDetails.classList.add('eachDetail');
         eachDetails.innerHTML = `
-        <div class="grid md:grid-cols-3 gap-4 md:mr-40 my-5">
-                    <div class="border border-slate-300 md:p-2">
-                        <div class=" flex justify-center">
-                            <img id="result-image" class="w-80 md:w-96 h-96"
-                                src="${details.data.image}" alt="">
-                        </div>
+        <div class="grid md:grid-cols-3 gap-0 my-5">
+                    <div class="border md:rounded-l-lg md:rounded-tr-none rounded-t-lg border-slate-300 bg-slate-50 md:p-2 p-3 flex md:justify-center justify-start content-center">
+                        <img id="result-image" class="w-auto h-auto" src="${details.data.image}" alt="">
                     </div>
-                    <div class="border border-slate-300 md:col-span-2 md:p-2">
-                        <div>
-                            <p id="phone-name" class="text-2xl font-normal md:px-3">${details.data.name}</p>
-                            <p id="release-date" class="text-lg font-normal md:px-3">Release date: ${details.data.releaseDate}</p>
+                    <div class="border border-slate-300 md:p-2 p-2">
+                            <p id="phone-name" class="text-2xl font-normal md:px-3 p-2">${details.data.name}</p>
+                            <p id="release-date" class="text-lg font-normal md:px-3 p-2">Release date: ${details.data.releaseDate}</p>
                             <hr>
-                            <p id="main-feature" class="text-xl font-normal md:px-3">Main features:</p>
-                            <div class="text-base">
-                                <p class="font-normal md:px-3">Storage: ${details.data.mainFeatures.storage}</p>
-                                <p class="font-normal md:px-3">Memory: ${details.data.mainFeatures.memory}</p>
-                                <p class="font-normal md:px-3">Display-size: ${details.data.mainFeatures.displaySize}</p>
-                                <p class="font-normal md:px-3">Chip-set: ${details.data.mainFeatures.chipSet}</p>
-                                <p class="font-normal md:px-3">Sensors:</p>
+                            <div class="text-lg font-base md:px-3 p-2">
+                            <p id="main-feature" class="text-xl font-normal">Main features:</p>
+                                <p>Storage: <span class="text-sm">${details.data.mainFeatures.storage}</span></p>
+                                <p>Memory: <span class="text-sm">${details.data.mainFeatures.memory}</span></p>
+                                <p>Display-size: <span class="text-sm">${details.data.mainFeatures.displaySize}</span></p>
+                                <p>Chip-set: <span class="text-sm">${details.data.mainFeatures.chipSet}</span></p>
+                                <p>Sensors:</p>
                             </div>
-                            <hr>
-                            <p id="others" class="text-xl font-normal md:px-3">Others:</p>
-                            <div class="text-base">
-                                <p class="font-normal md:px-3">Bluetooth: ${details.data.others.Bluetooth}</p>
-                                <p class="font-normal md:px-3">GPS: ${details.data.others.GPS}</p>
-                                <p class="font-normal md:px-3">NFC: ${details.data.others.NFC}</p>
-                                <p class="font-normal md:px-3">Radio: ${details.data.others.Radio}</p>
-                                <p class="font-normal md:px-3">USB: ${details.data.others.USB}</p>
-                                <p class="font-normal md:px-3">WLAN: ${details.data.others.WLAN}</p>
+                    </div>
+                    <div class="border border-slate-300 md:p-2 p-2">
+                            <div class="text-lg font-base md:px-3 px-2 pt-0">
+                            <p id="others" class="text-xl font-normal">Others:</p>
+                                <p>Bluetooth: <span class="text-sm">${details.data.others.Bluetooth}</span></p>
+                                <p>GPS: <span class="text-sm">${details.data.others.GPS}</span></p>
+                                <p>NFC: <span class="text-sm">${details.data.others.NFC}</span></p>
+                                <p>Radio: <span class="text-sm">${details.data.others.Radio}</span></p>
+                                <p>USB: <span class="text-sm">${details.data.others.USB}</span></p>
+                                <p>WLAN: <span class="text-sm">${details.data.others.WLAN}</span></p>
                             </div>
-                        </div>
                     </div>
         </div>
         `;
